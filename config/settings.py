6ebf1c4abf83694bd351983e31ca9375e4d227f6
Ruairidh MacArthur <roomacarthur@gmail.com>
2024-04-15 14:39:54 +0000

    Allowed Hosts updated allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.envrion.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-roomacarthur-apiproject-585d9kug047.ws-eu110.gitpod.io',]
 
 
 # Application definition
