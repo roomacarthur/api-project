4efcb87c02ebc81f45916034a9888fcbbed661a5
Ruairidh MacArthur <roomacarthur@gmail.com>
2024-04-15 14:21:48 +0000

    Initial Commit .2 additionall commit to catch unsaved file

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q6k6#k(z&11gy$6j#t&0txe@!8dm1u+nbit5o0+8i*vycwy*n$'
+SECRET_KEY = os.envrion.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
