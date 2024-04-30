4756a1e52e74a47bc330c0c582c29c986caba056
Ruairidh MacArthur <roomacarthur@gmail.com>
2024-04-30 16:05:33 +0000

    final touches pre deployment, added humanized time for comments, time format for posts and added route url and view

--- posts/models.py
+++ posts/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
     ]
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
     image = models.ImageField(upload_to='images/', default='../default_post_brequy', blank=True)
