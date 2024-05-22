3a65b2860f5261f9d9f1ea7abf6ddfcc44e06ea3
Ruairidh MacArthur <roomacarthur@gmail.com>
2024-05-22 13:42:28 +0000

    Update: cors fix for new development url

--- config/settings.py
+++ config/settings.py
@@ -111,9 +111,18 @@ MIDDLEWARE = [
 #     CORS_ALLOWED_ORIGIN_REGEXES = [
 #         r"^https://.*\.gitpod\.io$",
 #     ]
-CORS_ALLOWED_ORIGINS = [
-    "https://3000-roomacarthur-moments-ogwtotaczn0.ws-eu111.gitpod.io",
-]
+if 'CLIENT_ORIGIN' in os.environ:
+    CORS_ALLOWED_ORIGINS = [
+        os.environ.get('CLIENT_ORIGIN')
+    ]
+
+if 'CLIENT_ORIGIN_DEV' in os.environ:
+    extracted_url = re.match(
+        r'^.+-', os.environ.get('CLIENT_ORIGIN_DEV', ''), re.IGNORECASE
+    ).group(0)
+    CORS_ALLOWED_ORIGIN_REGEXES = [
+        rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
+    ]
 
 CORS_ALLOW_CREDENTIALS = True
 
