31171154160021bab07c97b56a8a7808ed917b3c
Ruairidh MacArthur <roomacarthur@gmail.com>
2024-05-22 16:22:50 +0000

    Update: settings.py to fix API calls

--- posts/models.py
+++ posts/models.py
@@ -28,15 +28,15 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
-    image = models.ImageField(upload_to='images/', default='../default_post_brequy', blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_rgq6aq', blank=True
+    )
     image_filter = models.CharField(
-        max_length=32,
-        choices=image_filter_choices,
-        default='normal'
+        max_length=32, choices=image_filter_choices, default='normal'
     )
 
     class Meta:
         ordering = ['-created_at']
-    
+
     def __str__(self):
         return f'{self.id} {self.title}'
